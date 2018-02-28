1e7b0bec2d59ff0c4cea84fd7d775072624feaaa
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2018-02-28 09:34:24 +0545

    Adds a empty AreaGraph

--- src/pingmonitor/Controller.java
+++ src/pingmonitor/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package pingmonitor;
 
 public class Controller {
 }
--- src/pingmonitor/Main.java
+++ src/pingmonitor/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package pingmonitor;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("mainview.fxml"));
+        primaryStage.setTitle("Ping Monitor");
+        primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
     }
 
